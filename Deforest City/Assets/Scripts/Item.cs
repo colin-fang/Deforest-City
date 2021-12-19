3d973d7dabc5c6ef3ca39e3877cf22b5b13389b1
colin-fang <46767117+colin-fang@users.noreply.github.com>
2021-12-19 10:52:27 -0500

    Scriptable Objects
    Scriptable Object item, and catalogue set up
    Scriptable Object Signal Object set up

---
+++ Deforest City/Assets/Scripts/Item.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu]
+public class Item : ScriptableObject
+{
+
+    public GameObject itemObject;
+    public string itemDescription;
+    public bool isPlant;
+
+}
